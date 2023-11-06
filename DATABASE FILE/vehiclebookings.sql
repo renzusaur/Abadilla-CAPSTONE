ffc5e5a236e7f0839be101df0200004c746ecec8
renzusaur <112527583+renzusaur@users.noreply.github.com>
2023-11-06 18:41:34 +0800

    Update vehiclebookings.sql

--- DATABASE FILE/vehiclebookings.sql
+++ DATABASE FILE/vehiclebookings.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2023 at 08:02 AM
+-- Generation Time: Nov 06, 2023 at 11:41 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -39,7 +39,7 @@ CREATE TABLE `tms_admin` (
 --
 
 INSERT INTO `tms_admin` (`a_id`, `a_name`, `a_email`, `a_pwd`) VALUES
-(1, 'System Admin', 'admin', 'admin\r\n');
+(1, 'System Admin', 'admin@mail.com', 'admin');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,9 @@ CREATE TABLE `tms_feedback` (
 INSERT INTO `tms_feedback` (`f_id`, `f_uname`, `f_content`, `f_status`) VALUES
 (1, 'Elliot Gape', 'This is a demo feedback text. This is a demo feedback text. This is a demo feedback text.', 'Published'),
 (2, 'Mark L. Anderson', 'Sample Feedback Text for testing! Sample Feedback Text for testing! Sample Feedback Text for testing!', 'Published'),
-(3, 'Liam Moore ', 'test number 3', '');
+(3, 'Liam Moore ', 'test number 3', ''),
+(4, 'Justin  Cristobal ', 'hellooooooo test ', 'Published'),
+(5, 'Justin  Cristobal ', 'Nag bayag ka noli mark', '');
 
 -- --------------------------------------------------------
 
@@ -79,7 +81,8 @@ CREATE TABLE `tms_pwd_resets` (
 --
 
 INSERT INTO `tms_pwd_resets` (`r_id`, `r_email`) VALUES
-(2, 'suckseed538@gmail.com');
+(2, 'suckseed538@gmail.com'),
+(3, 'suckseed538@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -123,17 +126,11 @@ CREATE TABLE `tms_user` (
 --
 
 INSERT INTO `tms_user` (`u_id`, `u_fname`, `u_lname`, `u_phone`, `u_addr`, `u_category`, `u_email`, `u_pwd`, `u_car_type`, `u_car_regno`, `u_car_bookdate`, `u_car_book_status`) VALUES
-(3, 'Demo', 'User', '070678909', '90100 Machakos ', 'Driver', 'demouser@tms.com', 'demo123', 'SUV', 'CA1001', '2022-09-01', 'Approved'),
-(4, 'John', 'Settles', '7145698540', '45 Clearview Drive', 'Driver', 'johns@mail.com', 'password', '', '', '', ''),
-(5, 'Joseph', 'Yung', '7896587777', '72 Doe Meadow Drive', 'Driver', 'joseph@mail.com', 'password', '', '', '', ''),
-(6, 'Vincent', 'Pelletier', '4580001456', '58 Farland Avenue', 'Driver', 'vincentp@mail.com', 'password', '', '', '', ''),
-(7, 'Jesse', 'Robinson', '1458887855', '73 Fleming Way', 'Driver', 'jesser@mail.com', 'password', '', '', '', ''),
-(8, 'Nelson', 'Ford', '7458965874', '58 West Side Avenue', 'User', 'nelford@mail.com', 'password', 'Sedan', 'CA1690', '2022-09-13', 'Approved'),
-(9, 'Paul', 'Mills', '7412563258', '12 Red Maple Drive', 'User', 'paul@mail.com', 'password', 'Sedan', 'CA2077', '2022-09-14', 'Approved'),
-(10, 'Liam', 'Moore', '7410001212', '114 Bleck Street', 'User', 'liamoore@mail.com', 'password', 'Sedan', 'CA1690', '2022-09-14', 'Approved'),
-(11, 'Jeff', 'Lewis', '7854545454', '114 Test Adr', 'User', 'jeff@mail.com', 'password', 'Sedan', 'CA7700', '2022-09-14', 'Pending'),
-(12, 'Kenya', 'Norman', '7896547855', '114 Test Addr', 'User', 'normank@mail.com', 'password', 'Bus', 'CA7766', '2022-09-15', 'Pending'),
-(13, 'Justin ', 'Cristobal', '09776964988', 'Brgy 10 San Nicolas', 'User', 'suckseed538@gmail.com', 'Justinrenz28', '', '', '', '');
+(13, 'Justin ', 'Cristobal', '09776964988', 'Brgy 10 San Nicolas', 'User', 'suckseed538@gmail.com', 'HiraiMomo09', 'Exclusive', 'P1,200', '2023-11-07', 'Approved'),
+(14, 'Noli Mark', 'Corpuz', '09776964988', 'Brgy 14 San Nicolas', 'User', 'nolimarkcorpuz@gmail.com', 'nolimark', '', '', '', ''),
+(15, 'jm ', 'cristobal', '23213213213', 'Brgy 10 San Nicolas', 'User', 'jmcristobal0729@gmail.com', 'JMBAMBOO29', 'Exclusive', 'P200/head', '2023-11-06', 'Approved'),
+(17, 'Justin ', 'Cristobal', '09776964988', 'Brgy 10 San Nicolas', 'Driver', 'suckseed538@gmail.com', 'asdasd', '', '', '', ''),
+(19, 'Melissa Joy', 'Cristobal', '09080808080', 'Brgy 10 San Nicolas', 'User', 'melissa@gmail.com', 'Kariotics11', 'Discounted', 'P800', '2023-11-07', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -157,11 +154,12 @@ CREATE TABLE `tms_vehicle` (
 --
 
 INSERT INTO `tms_vehicle` (`v_id`, `v_name`, `v_reg_no`, `v_pass_no`, `v_driver`, `v_category`, `v_dpic`, `v_status`) VALUES
-(3, 'Euro Bond', 'CA7766', '50', 'Vincent Pelletier', 'Bus', 'buscch.jpg', 'Available'),
-(4, 'Honda Accord', 'CA2077', '5', 'Joseph Yung', 'Sedan', '2019_honda_accord_angularfront.jpg', 'Available'),
-(5, 'Volkswagen Passat', 'CA1690', '5', 'Jesse Robinson', 'Sedan', 'volkswagen-passat-500.jpg', 'Available'),
-(6, 'Nissan Rogue', 'CA1001', '7', 'Demo User', 'SUV', 'Nissan_Rogue_SV_2021.jpg', 'Available'),
-(7, 'Subaru Legacy', 'CA7700', '5', 'John Settles', 'Sedan', 'Subaru_Legacy_Premium_2022_2.jpg', 'Available');
+(3, 'Pool Only', 'P200/head', 'N/A', 'N/A', 'Exclusive', 'buscch.jpg', 'Available'),
+(4, 'Cottage03', 'P600', 'Mushroom Type', '1-3pax', 'Exclusive', '2019_honda_accord_angularfront.jpg', 'Available'),
+(5, 'Cottage04', 'P800', 'Nipa Type', '3-5pax', 'Discounted', 'chhh.jpg', 'Booked'),
+(6, 'Cottage01', 'P600 ', 'Mushroom Type', '1-3pax', 'Exclusive', 'Nissan_Rogue_SV_2021.jpg', 'Available'),
+(7, 'Cottage02', 'P600', 'Mushroom Type', '1-3pax', 'Exclusive', 'Subaru_Legacy_Premium_2022_2.jpg', 'Available'),
+(8, 'Cottage05', 'P1,200', 'Nipa Type', '6-8pax', 'Exclusive', '', 'Booked');
 
 --
 -- Indexes for dumped tables
@@ -217,13 +215,13 @@ ALTER TABLE `tms_admin`
 -- AUTO_INCREMENT for table `tms_feedback`
 --
 ALTER TABLE `tms_feedback`
-  MODIFY `f_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `f_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tms_pwd_resets`
 --
 ALTER TABLE `tms_pwd_resets`
-  MODIFY `r_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `r_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tms_syslogs`
@@ -235,13 +233,13 @@ ALTER TABLE `tms_syslogs`
 -- AUTO_INCREMENT for table `tms_user`
 --
 ALTER TABLE `tms_user`
-  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tms_vehicle`
 --
 ALTER TABLE `tms_vehicle`
-  MODIFY `v_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `v_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
